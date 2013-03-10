de5fb396e131839a2ac731c465e001cb46df6094
Tarek Ziade <tarek@ziade.org>
2013-03-10 09:01:27 +0100

    added a modal to change the dates

--- grenouille/web/app.js
+++ grenouille/web/app.js
@@ -2,8 +2,8 @@
 
 var temp = new GrenouilleAggregate("temp",
                         "http://0.0.0.0:9901/weather/_search",
-                        "#startdate-temp",
-                        "#enddate-temp",
+                        "#startdate",
+                        "#enddate",
                         "chart-temp",
                         "Temperature",
                         "temperature",
@@ -13,8 +13,8 @@ temp.draw();
 
 var hum = new GrenouilleAggregate("hum",
                         "http://0.0.0.0:9901/weather/_search",
-                        "#startdate-hum",
-                        "#enddate-hum",
+                        "#startdate",
+                        "#enddate",
                         "chart-hum",
                         "Humidity",
                         "humidity",
@@ -24,11 +24,25 @@ hum.draw();
 
 var press = new GrenouilleAggregate("press",
                         "http://0.0.0.0:9901/weather/_search",
-                        "#startdate-press",
-                        "#enddate-press",
+                        "#startdate",
+                        "#enddate",
                         "chart-press",
                         "Pressure",
                         "pressure",
                         "hour");
 
 press.draw();
+
+$('#startdate').datepicker();
+$('#startdate').datepicker().on('changeDate',
+      function(ev) {$('#startdate').datepicker('hide')});
+
+$('#enddate').datepicker();
+$('#enddate').datepicker().on('changeDate',
+      function(ev) {$('#enddate').datepicker('hide')});
+
+function refreshChart() {
+  temp.draw(); 
+  hum.draw();
+  press.draw();
+}
